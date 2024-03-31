9506cba5b86a6c00ef78d84b847647243aa570aa
Adil327 <adilsajjad192@gmail.com>
2024-03-31 20:18:43 +0500

    Tweaks in Scripts

--- javascript/script.js
+++ javascript/script.js
@@ -1,47 +1,47 @@
 let cities = ["Faisalabad", "Lahore", "Karachi", "Islamabad", "Peshawar", "Shaikhupura", "Kashmir"]
 
-// Clear Output Button 
-function clearOutput() {
-    document.getElementById("output").innerHTML = " "
+// Clear Output Button
+function clearOutput(){
+    document.getElementById("output-div").innerHTML = " ";
 }
 
 // Simple Alert 
 function simpleAlert() {
-    alert("Alert Button is Clicked")
-    document.getElementById("output").innerHTML = "Alert button is clicked"
+    alert("Alert Button is Clicked");
+    document.getElementById("output-div").innerHTML = "Alert button is Clicked";
 }
 
 // Print My Name 
 function printName() {
-    let userName = prompt("please enter your name")
-    document.getElementById("output").innerHTML = userName;
+    let userName = prompt("Enter Your Name Please");
+    document.getElementById("output-div").innerHTML = userName;
 }
 
 // Print All Cities 
 function printAllCities() {
-    document.getElementById("output").innerHTML = " "
+    document.getElementById("output-div").innerHTML = " ";
     for (let i = 0; i < cities.length; i++) {
-        document.getElementById("output").innerHTML += i + 1 + ") " + cities[i] + "<br />"
+        document.getElementById("output-div").innerHTML += i + 1 + ") " + cities[i] + "<br />"
     }
 }
 
 // Add Cities in the List  
 function addCity() {
-    document.getElementById("output").innerHTML = " "
-    let newCity = prompt("please enter city name you want to add")
-    cities.push(newCity)
+    document.getElementById("output-div").innerHTML = " ";
+    let newCity = prompt("Please Enter the City Name You Want to Add");
+    cities.push(newCity);
 
     for (let i = 0; i < cities.length; i++) {
-        document.getElementById("output").innerHTML += i + 1 + ")" + cities[i] + "<br />"
+        document.getElementById("output-div").innerHTML += i + 1 + ")" + cities[i] + "<br />"
     }
 }
 
 // Generate Table
 function generateTable() {
-    tableNumber = +prompt("please enter table number that use want to generate")
-    document.getElementById("output").innerHTML = `<h3>Table of ${tableNumber}<h3/> <hr />`
+    tableNumber = +prompt("Please Enter The Table Number That You Want to Generate")
+    document.getElementById("output-div").innerHTML = `<h3>Table of ${tableNumber}<h3/> <hr />`
     for (let i = 1; i < 11; i++) {
         let table = tableNumber + " * " + i + " = " + tableNumber * i + "<br />"
-        document.getElementById("output").innerHTML += table;
+        document.getElementById("output-div").innerHTML += table;
     }
 }
